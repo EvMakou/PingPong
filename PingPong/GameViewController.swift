9c3d2796ed128bad0e282355266725eba467748d
supermacho <face_buk@mail.ru>
2017-09-08 14:04:06 +0300

    First commit

--- PingPong/GameViewController.swift
+++ PingPong/GameViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+
+
+var currentGameType = gameType.medium
+
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -21,6 +25,9 @@ class GameViewController: UIViewController {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
+                scene.size = view.bounds.size
+                
+                
                 // Present the scene
                 view.presentScene(scene)
             }
@@ -31,7 +38,7 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
